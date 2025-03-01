c370f8a3c1cd6440c44264de16ec99618546650b
Karim Ourrai <karim.ourrai@gmail.com>
2025-03-01 03:11:00 +0000

    Remove unused dependencies and files; update author meta in index.html

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,36 @@ import { ThemeProvider } from "next-themes";
 import Navigation from "./components/Navigation";
 import ParticleGrid from "./components/ParticleGrid";
 import Footer from "./components/Footer";
-import Home from "./pages/Home.tsx";
-import Experience from "./pages/Experience.tsx";
-import Education from "./pages/Education.tsx";
-import Skills from "./pages/Skills.tsx";
-import Contact from "./pages/Contact.tsx";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import Education from "./pages/Education";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 import './i18n/i18n';
 
-const App = () => {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-      <div className="bg-[#1a1e23] relative">
-        <ParticleGrid />
-        <Navigation />
-        <section id="home">
-          <Home />
-        </section>
-        <section id="skills">
-          <Skills />
-        </section>
-        <section id="experience">
-          <Experience />
-        </section>
-        <section id="education">
-          <Education />
-        </section>
-        <section id="contact">
-          <Contact />
-        </section>
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
-};
+const App = () => (
+  <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <div className="bg-[#1a1e23] relative">
+      <ParticleGrid />
+      <Navigation />
+      <section id="home">
+        <Home />
+      </section>
+      <section id="skills">
+        <Skills />
+      </section>
+      <section id="experience">
+        <Experience />
+      </section>
+      <section id="education">
+        <Education />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <Footer />
+    </div>
+  </ThemeProvider>
+);
 
 export default App;
